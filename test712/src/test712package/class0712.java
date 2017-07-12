89bbfa22352c1911704741d8557a694c44755d68
kelisipao <643289817@qq.com>
2017-07-12 08:49:25 +0800

    alter class0712.java

--- test712/src/test712package/class0712.java
+++ test712/src/test712package/class0712.java
@@ -3,5 +3,6 @@ package test712package;
 public class class0712 {	
 	public static void main(String[] args) {
 	System.out.println("11111111111111111");
+	System.out.println("222222222222222222");
  }
 }
